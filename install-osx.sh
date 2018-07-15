0c1991d6dc5ee6361755ede541b4cff93f6cbe38
Sarah Robin <hello@sarahrobin.net>
2018-07-15 15:59:46 -0700

    adds osx install script

---
+++ install-osx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get dotfiles installation directory
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf "$DOTFILES_DIR/.gitconfig" ~
+ln -sf "$DOTFILES_DIR/.gitignore_global" ~
