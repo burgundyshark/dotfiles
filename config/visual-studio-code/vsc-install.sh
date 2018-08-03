e56bc9559459a6ba7740206d7cb36c30497e211a
burgundyshark <burgundyshark@protonmail.com>
2018-08-02 22:04:50 -0700

    vsc debugging

--- config/visual-studio-code/vsc-install.sh
+++ config/visual-studio-code/vsc-install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-source ../../lib.sh
+# source ../../lib.sh
 
-bot "Installing Visual Studio Code extensions and config."
+echo "Installing Visual Studio Code extensions and config."
 
 code -v > /dev/null
 if [[ $? -eq 0 ]];then
     read -r -p "Do you want to install VSC extensions? [y|N] " configresponse
     if [[ $configresponse =~ ^(y|yes|Y) ]];then
-        ok "Installing extensions please wait..."    
+        echo "Installing extensions please wait..."    
             
         # Extensions
         code --install-extension formulahendry.auto-close-tag
@@ -17,7 +17,7 @@ if [[ $? -eq 0 ]];then
         code --install-extension editorconfig.editorconfig
         code --install-extension waderyan.gitblame
         code --install-extension wix.vscode-import-cost
-        code --install-extension twentychung.jsx
+        #code --install-extension twentychung.jsx
         code --install-extension christian-kohler.npm-intellisense
         code --install-extension christian-kohler.path-intellisense
         code --install-extension sandcastle.whitespace
@@ -45,25 +45,25 @@ if [[ $? -eq 0 ]];then
         code --install-extension daylerees.rainglow
         code --install-extension nopjmp.fairyfloss
 
-        ok "Extensions for VSC have been installed. Please restart your VSC."
+        echo "Extensions for VSC have been installed. Please restart your VSC."
     else
-        ok "Skipping extension install.";
+        echo "Skipping extension install.";
     fi
 
     read -r -p "Do you want to overwrite user config? [y|N] " configresponse
     if [[ $configresponse =~ ^(y|yes|Y) ]];then
         read -r -p "Do you want to back up your current user config? [Y|n] " backupresponse
         if [[ $backupresponse =~ ^(n|no|N) ]];then
-            ok "Skipping user config save."
+            echo "Skipping user config save."
         else
             cp $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.backup.json
-            ok "Your previous config has been saved to: $HOME/Library/Application Support/Code/User/settings.backup.json"
+            echo "Your previous config has been saved to: $HOME/Library/Application Support/Code/User/settings.backup.json"
         fi
         cp ./settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
-        ok "New user config has been written. Please restart your VSC."
+        echo "New user config has been written. Please restart your VSC."
     else
-        ok "Skipping user config overwriting.";
+        echo "Skipping user config overwriting.";
     fi
 else
     error "It looks like the command 'code' isn't accessible."
--- install-osx.sh
+++ install-osx.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-bot "~ Hello, world! ~"
-bot "Lets get your dotfiles configured on OSX"
+echo "~ Hello, world! ~"
+echo "Lets get your dotfiles configured on OSX"
 # Get dotfiles installation directory
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-bot "Hrm. Looks like we'll be copying my dotfiles to $DOTFILES_DIR!"
-bot ""
-bot "First, let's get your git configured..."
+echo "Hrm. Looks like we'll be copying my dotfiles to $DOTFILES_DIR!"
+echo ""
+echo "First, let's get your git configured..."
 ln -sf "$DOTFILES_DIR/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/.gitignore_global" ~
-bot "Awesome! Next up, Visual Studio Code"
+echo "Awesome! Next up, Visual Studio Code"
+# TODO - this needs sudo?
 ./config/visual-studio-code/vsc-install.sh
-bot "And you're good to code!"
-bot ""
-bot "Okay, that looks like all of it! Have a nice day ~ ! <3"
+echo "And you're good to code!"
+echo ""
+echo "Okay, that looks like all of it! Have a nice day ~ ! <3"
